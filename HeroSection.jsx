c3bb548adfdd97e9af121e1ffa4fc685071d0df7
Fkarau <30944677+Karau1218@users.noreply.github.com>
2026-04-14 11:35:52 -0700

    added code to herosection jxs

--- HeroSection.jsx
+++ HeroSection.jsx
@@ -1,3 +1,22 @@
 // this section is the starting page that visistors
 // see when they open the page
 // add it to app.jxs
+
+export default function Main() {
+    return (
+        // IS IT MAIN OR DIV
+        <div>  
+     3   <h1> Why I'm learning React</h1>
+        {/* <ul className ="firstPage"> */}
+        <p>I'm learning React to improve my skills in coding, 
+            build modern applications
+        </p>
+        <ul>
+            <li>Build Real World applications</li>
+            <li>Improve front end skills</li>
+            <li>Prepare for tech jobs</li>
+            <li>Learn modern tools</li>
+        </ul>
+        </div>
+        )
+    }
